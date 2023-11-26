db718f8b412f7a79b4c3f906322c6e856437a48a
Biniyam Belay <binibelaygom@gmail.com>
2023-11-26 16:18:33 +0300

    wrote the code for grid generator, modified the css, and now working on the button to take the grid size from the user input

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
-let divs
-let i = 0
-for (i = 1; i < 257; i++){
-    divs = document.createElement('div');
-    divs.style.cssText = 'width: 20px; height: 20px; background: red; flex: 0 0 calc(6.25% - 1px);'
+// a function that changes the color of the box n hover
+function handleMouseOver(event) {
+  // Change the color to yellow
+  event.target.style.backgroundColor = "#E77728";
+}
 
-    divs.addEventListener('mouseover', () =>{
-        divs.style.background = "yellow"
-    })
+// creating the boxes
+for (i = 0; i < 256; i++) {
+  divs = document.createElement("div");
+  divs.classList.add("divv");
+  divs.style.cssText =
+    "width: 40px; height: 30px; background: #EDB230; flex: 0 0 calc(6.25% - 1px);";
 
-    container.appendChild(divs)
+  divs.addEventListener("mouseover", handleMouseOver);
+
+  container.appendChild(divs);
 }
